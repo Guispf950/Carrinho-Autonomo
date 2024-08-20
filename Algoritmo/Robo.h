0f903583bda552eae123d122929d4cfebfc611b2
Guilherme Pereira <guisp950@gmail.com>
2024-08-20 10:44:37 -0400

    Update Robo.h

--- Algoritmo/Robo.h
+++ Algoritmo/Robo.h
@@ -7,11 +7,12 @@ class Robo {
     float y;
     float theta;
   public:
-     Robo() : x(0), y(0), theta(0) {}  // Construtor padrão
+    Robo() : x(0), y(0), theta(0) {}  // Construtor padrão
     Robo(float x, float y, float theta); // Declaração do construtor
     float GetX(); // Declaração dos métodos
     float GetY();
     float GetTheta();
+    float GetAtualTheta();
     void SetX(float x1); // Adiciona parâmetros
     void SetY(float y1); // Adiciona parâmetros
     void SetTheta(float theta1); // Adiciona parâmetros
